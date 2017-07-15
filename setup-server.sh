5baa1eec72f477dc9f8960aef8a640aa18d14361
Marcelo Lacerda <marceloslacerda@gmail.com>
2017-07-15 10:26:38 -0300

    Move static files improve installation

--- setup-server.sh
+++ setup-server.sh
@@ -1,9 +1,18 @@
 #!/bin/sh -e
+INSTALL_DIR=/usr/local/cody/
+CODE_DIR=$INSTALL_DIR/code/
+CURRENT_DIR=$(dirname $0)
 
 apt-get install nginx uwsgi uwsgi-plugin-python3 python3-flask
-mkdir -p code/
-chgrp -R www-data code/
-chmod -R g+rw code/
+rm -rf $INSTALL_DIR
+cp -r $CURRENT_DIR $INSTALL_DIR
+mkdir -p $CODE_DIR
+chgrp -R www-data $CODE_DIR
+chmod -R g+rw $CODE_DIR
+rm /var/www/html/cody
+ln -s $INSTALL_DIR/static/ /var/www/html/cody
+cp cody.service /etc/systemd/system/
+
 echo 'Add this to nginx'
 echo '       location = /storage { rewrite ^ /storage/; }'
 echo '       location /storage { try_files $uri @storage; }'
--- start-storage.sh
+++ start-storage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uwsgi -s /tmp/storage.sock --manage-script-name --mount /storage=storage:app --plugin python3 --uid www-data --gid www-data --master --processes 2
+uwsgi -s /tmp/storage.sock --manage-script-name --mount /storage=storage:app --plugin python3 --uid www-data --gid www-data --master --processes 2 --vacuum --die-on-term
